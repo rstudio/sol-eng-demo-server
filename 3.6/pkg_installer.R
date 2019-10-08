77e00ad59e6c72e72cae8bd74197253f28ff2d2a
Alex Gold <alexkgold@gmail.com>
2019-10-07 23:03:00 -0400

    Add working Dockerfile

--- 3.6/pkg_installer.R
+++ 3.6/pkg_installer.R
@@ -1,10 +1,17 @@
 docker_pkg_install <- function(package_csv, install_loc) {
   pkgs <- read.csv(package_csv, stringsAsFactors = FALSE)
 
-  print("INSTALLING PAK")
-  utils::install.packages("pak")
+  #utils::install.packages("pak")
 
-  pak::pkg_install(pkgs$package, lib = install_loc)
+  utils::install.packages('future')
+  ncores <- max(1, future::availableCores())
+
+  print(sprintf("Installing on %s cores", ncores - 1))
+  options(Ncpus = ncores)
+
+  utils::install.packages(pkgs$package, lib = install_loc)
+
+
   # try_package <- function(package, ...) {
   #   tryCatch({
   #     pak::pkg_install(package, lib = install_loc)
