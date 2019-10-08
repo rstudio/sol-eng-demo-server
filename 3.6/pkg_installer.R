e46ce0fc6848d2051f08063038152dcfc901b0ca
Alex Gold <alexkgold@gmail.com>
2019-10-07 23:08:59 -0400

    remove pak

--- 3.6/pkg_installer.R
+++ 3.6/pkg_installer.R
@@ -2,9 +2,9 @@ docker_pkg_install <- function(package_csv, install_loc) {
   pkgs <- read.csv(package_csv, stringsAsFactors = FALSE)
 
   utils::install.packages('future')
-  ncores <- max(1, future::availableCores())
+  ncores <- max(1, future::availableCores() - 1)
 
-  print(sprintf("Installing on %s cores", ncores - 1))
+  print(sprintf("Installing on %s cores", ncores))
   options(Ncpus = ncores)
 
   utils::install.packages(pkgs$package, lib = install_loc)
